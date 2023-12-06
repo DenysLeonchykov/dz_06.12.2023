17b67cdca60484819ff85e573883ea69a55fa775
DenysLeonchykov <149931093+DenysLeonchykov@users.noreply.github.com>
2023-12-06 14:43:16 +0300

    Виконано

---
+++ app.js
@@ -0,0 +1,24 @@
+ 
+let div = document.querySelector('.block2');
+const rightBtn = document.querySelector('.right');
+const leftBtn = document.querySelector('.left');
+
+let left = 200;
+const blokAll = 600
+
+
+rightBtn.addEventListener('click', () => {
+    left+= 10;
+    div.style.left = `${left}px`;
+    if (blokAll - left < 200) {
+        rightBtn.setAttribute('disabled', '');
+    }
+})
+
+leftBtn.addEventListener('click', () => {
+    left-= 10;
+    div.style.left = `${left}px`;
+    if (left === 0) {
+        leftBtn.setAttribute('disabled', '');
+    }
+})
